a6a399c7daa99b4576d1e9c6484e73464c31aed2
Matt Brown <mattbrown@spotify.com>
2018-10-15 15:21:05 -0400

    working on JAVA_HOME of 11

---
+++ src/main/java/com/spotify/example/Main.java
@@ -0,0 +1,17 @@
+package com.spotify.example;
+
+/** Application entry point. */
+public final class Main {
+  private Main() {}
+
+  public static void main(final String... args) {
+    final String[] x = new String[] {"one"};
+    final String[] y = new String[] {"two"};
+    if (x.equals(y)) {
+      System.out.println("arrays are equal!");
+    }
+    new Exception();
+
+    System.out.println("goodbye!");
+  }
+}
