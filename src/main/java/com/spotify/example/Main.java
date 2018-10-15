baf933f9920ad2dd81a6791952c2e09a05ee23b6
Matt Brown <mattbrown@spotify.com>
2018-10-15 16:17:09 -0400

    remove purposeful error

--- src/main/java/com/spotify/example/Main.java
+++ src/main/java/com/spotify/example/Main.java
@@ -5,12 +5,7 @@ public final class Main {
   private Main() {}
 
   public static void main(final String... args) {
-    final String[] x = new String[] {"one"};
-    final String[] y = new String[] {"two"};
-    if (x.equals(y)) {
-      System.out.println("arrays are equal!");
-    }
-    new Exception();
+    System.out.println("hello!");
 
     System.out.println("goodbye!");
   }
